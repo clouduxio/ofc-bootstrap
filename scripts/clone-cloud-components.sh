82a097c9198c73d02309d047d9a15d146233f729
Ante Grgic <ante16@gmail.com>
2020-01-04 17:28:40 -0600

    fixed registry and component clone script

--- scripts/clone-cloud-components.sh
+++ scripts/clone-cloud-components.sh
@@ -5,5 +5,5 @@ rm -rf ./tmp/openfaas-cloud
 git clone https://github.com/clouduxio/openfaas-cloud ./tmp/openfaas-cloud
 
 cd ./tmp/openfaas-cloud
-echo "Checking out openfaas/openfaas-cloud@$TAG"
+echo "Checking out clouduxio/openfaas-cloud@$TAG"
 git checkout $TAG
